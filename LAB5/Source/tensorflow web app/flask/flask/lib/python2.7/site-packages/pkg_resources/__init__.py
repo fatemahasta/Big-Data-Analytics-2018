d93dd28ebb00f078e788a9107b8c5acd92ed4f36
Your Name <you@example.com>
2018-04-04 19:53:42 -0500

    updated

---
+++ LAB5/Source/tensorflow web app/flask/flask/lib/python2.7/site-packages/pkg_resources/__init__.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e47748959ed0c973dbac3376a9746c556a4c40b6de2d93209e2217e6ae162aae
+size 101373
---
+++ LAB5/Source/tensorflow web app/flask/flask/lib/python2.7/site-packages/pkg_resources/_vendor/packaging/__about__.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cee6e32b222a36e19d827bb0a65e82fdcb4cc7e667e782210fa7a3374119694d
+size 720
